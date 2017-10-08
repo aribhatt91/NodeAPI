63c1bbc93357f3623280a07769af016fd6c2181e
aribhatt91 <aribhatt91@users.noreply.github.com>
2017-10-08 23:54:28 +0530

    Added express

--- config/express-helper.js
+++ config/express-helper.js
@@ -3,3 +3,23 @@ var app = express();
 var router = express.Router();
 
 app.listen(3000);
+
+function _respond(res, next, status, data, http_code) {
+    var response = {
+        'status': status,
+        'status-code': http_code,
+        'data': data
+    };
+    res.setHeader('content-type', 'application/json');
+    res.writeHead(http_code);
+    res.send(JSON.stringify(response));
+    return next();
+}
+
+module.exports.success = function(res, next, data){
+    _respond(res, next, 'success', data, 200);
+}
+
+module.exports.failure = function(res, next, data, http_code){
+    _respond(res, next, 'failure', data, http_code);
+}
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -21,25 +21,45 @@ module.exports = function (server) {
     });
 
     server.post('/user', function (req, res, next) {
-        var user = req.body;
-        max_user_id++;
-        user['id'] = max_user_id;
-        users[user.id] = user;
-
-        helper.success(res, next, user);
+        req.assert('first_name', 'First name is required').notEmpty()
+        req.assert('last_name', 'Last name is required').notEmpty();
+        req.assert('email', 'Email id is required').notEmpty().isEmail();
+        req.assert('career', 'Career must be either student or teadcher').notEmpty().isIn(['student', 'teacher', 'professor']);
+        var errors = req.validationErrors();
+        if (errors) {
+            helper.failure(res, next, errors, 400);
+        } else {
+            var user = req.body;
+            max_user_id++;
+            user['id'] = max_user_id;
+            users[user.id] = user;
+            helper.success(res, next, user);
+        }
     });
 
 
     server.put('/user/:id', function (req, res, next) {
+        req.assert('id', 'Id is required and must be numeric').notEmpty().isInt();
+        var errors = req.validationErrors();
         var user = users[parseInt(req.params.id)];
-        var update = req.body;
-        update['id'] = user['id'];
-        users[user.id] = user;
-        helper.success(res, nexxt, update);
+        if (errors || typeof user === 'undefined') {
+            helper.failure(res, next, 'The specified user couldn\'t be found', 404);
+        } else {
+            var update = req.body;
+            for (var field in update) {
+                user[field] = update[field];
+            }
+            //users[user.id] = user;
+            helper.success(res, nexxt, user);
+        }
     });
 
     server.del('/user/:id', function (res, req, next) {
-        delete users[parseInt(res.params.id)];
-        helper.success(res, next, users);
+        if (typeof user === 'undefined') {
+            helper.failure(res, next, 'The specified user couldn\'t be found', 404);
+        } else {
+            delete users[parseInt(res.params.id)];
+            helper.success(res, next, users);
+        }
     });
-}
+};
